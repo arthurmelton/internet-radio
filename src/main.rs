180587b9aacc5d57a1ee63ce0d1e117ac58dfaad
Arthur Melton <amtitan@icloud.com>
2022-11-14 08:24:45 -0500

    start to get working

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate lazy_static;
+
+mod config;
+mod scan;
+use config::ARGS;
+use scan::open;
+
+#[tokio::main]
+async fn main() {
+    open().await;
 }
